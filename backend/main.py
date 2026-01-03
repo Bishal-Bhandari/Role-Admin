5e88181dc5b426f3a907790c44858e07f27e9191
bishal <bishalbhaandari62@gmail.com>
2026-01-03 10:16:22 +0100

    Debugging

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from config import app, db
-from models import ContextDecorator
+from models import Contact
 
 @app.route("/contacts", methods=["GET"])
 def get_contacts():
--- backend/models.py
+++ backend/models.py
@@ -7,7 +7,8 @@ class Contact(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def to_json(self):
-        retun{
+        return
+        {
             "id": self.id,
             "firstName": self.first_name,
             "lastName": self.last_name,
