0393a05990e374e794eead444595d85c3cb03f53
bishal <bishalbhaandari62@gmail.com>
2026-01-07 13:44:34 +0100

    Add table headers and actions to ContactList component

--- frontend/src/ContactList.jsx
+++ frontend/src/ContactList.jsx
@@ -4,7 +4,28 @@ const ContactList = ({ contacts }) => {
     return <div>
         <h2>Contacts</h2>
         <table>
-            <thead></thead>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>  
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {contacts.map((contact) => (
+                    <tr key={contact.id}>
+                        <td>{contact.firstName}</td>
+                        <td>{contact.lastName}</td>
+                        <td>{contact.email}</td>
+                        <td>
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
         </table>
     </div>
 }
+export default ContactList
