a6fe8b6335c030146e7929bc9fa12a8c7f9fa3a9
bishal <bishalbhaandari62@gmail.com>
2026-01-07 13:36:29 +0100

    ADDING FORNEND ROUTE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState([])
+
+  useEffect(() => {
+    fetchContacts()
+  }, [])
+
+  const fetchContacts = async () => {
+    const response = await fetch('http://localhost:8000/contacts/')
+    const data = await response.json()
+    setContacts(data.contacts)
+    console.log(data.contacts)
+  }
+  
   return (
     <>
       
---
+++ frontend/src/ContactList.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const ContactList = ({ contacts }) => {
+    return <div>
+        <h2>Contacts</h2>
+        <table>
+            <thead></thead>
+        </table>
+    </div>
+}
