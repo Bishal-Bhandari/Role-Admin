64af1c72f27a8b0b968ce3bc2b5168405e688947
bishal <bishalbhaandari62@gmail.com>
2026-01-07 11:30:57 +0100

    Update req done

--- backend/main.py
+++ backend/main.py
@@ -27,6 +27,22 @@ def create_contact():
     
     return jsonify({"message": "User created!"}), 201
 
+@app.route("/update_contact/<int:id>", methods=["PUT"])
+def update_contact(id):
+    contact = Contact.query.get(id)
+
+    if not contact:
+        return jsonify({"message": "Contact not found."}), 404
+
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+
+    db.session.commit()
+    return jsonify({"message": "Contact updated."}), 200
+
+
 
 @app.route("/delete_contact/<int:id>", methods=["DELETE"])
 def delete_contact(id):
