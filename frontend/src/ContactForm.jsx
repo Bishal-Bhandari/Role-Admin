e980607e31b230fa2c5db3d53c16f3c1d3e6bb5a
bishal <bishalbhaandari62@gmail.com>
2026-01-07 15:13:14 +0100

    Add ContactForm component and basic styling for table headers

---
+++ frontend/src/ContactForm.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+const ContactFrom = ({}) => {
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+
+    const onSubmit = async(e) => {
+        e.preventDefault()
+
+        const data = {
+            firstName,
+            lastName,
+            email
+        }
+        const url = "http://127.0.0.1.5000/create_contact"
+    }
+
+    return (<form>
+        <div>
+            <label htmlFor="firstName">First Name:</label>
+            <input 
+            type ="text"
+            id = "firstName"
+            value = {firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            />
+        </div>
+        <div>
+            <label htmlFor="lastName">Last Name:</label>
+            <input 
+            type ="text"
+            id = "lastName"
+            value = {lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            />
+        </div>
+        <div>
+            <label htmlFor="email">Email:</label>
+            <input 
+            type ="text"
+            id = "email"
+            value = {email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
+        </div>
+        <button type="submit">Add Contact</button>
+    </form>
+    );
+};
